66d92e9f040443f9d0551a88bba915ec4496e90e
elftausend <skatsacc.gut24@gmail.com>
2022-05-24 07:19:47 +0200

    test

--- tests/fns.rs
+++ tests/fns.rs
@@ -1,4 +1,4 @@
-use custos::{cpu::CPU, number::Float, AsDev, Matrix};
+use custos::{cpu::CPU, number::Float, AsDev, Matrix, CLDevice};
 use custos_math::FnsOps;
 
 pub fn roughly_equals<T: Float>(lhs: &[T], rhs: &[T], diff: T) {
@@ -27,4 +27,28 @@ fn test_fns_cpu() {
 
     let res = device.ln(&x);
     roughly_equals(&res.read(), &[0.405, 0.6931, 1.7917, f64::NAN], 0.001);
+
+    let res = device.powf(&x, 2.);
+    roughly_equals(&res.read(), &[2.25, 4., 36., 9.,], 0.001);
+}
+
+#[test]
+fn test_fns_cl() -> Result<(), custos::Error> {
+    let device = CLDevice::get(0)?.select();
+
+    let x = Matrix::from((&device, (1, 4), [1.5, 2., 6., -3.]));
+
+    let res = device.neg(&x);
+    assert_eq!(res.read(), vec![-1.5, -2., -6., 3.]);
+
+    let res = device.exp(&x);
+    roughly_equals(&res.read(), &[4.4816, 7.3891, 403.4288, 0.04979], 0.001);
+
+    let res = device.ln(&x);
+    roughly_equals(&res.read(), &[0.405, 0.6931, 1.7917, f32::NAN], 0.001);
+
+    let res = device.powf(&x, 2.);
+    roughly_equals(&res.read(), &[2.25, 4., 36., 9.,], 0.001);
+
+    Ok(())
 }
