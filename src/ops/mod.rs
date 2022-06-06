38d0244c23fda891530502d1f982ecd0be1cfdcf
elftausend <skatsacc.gut24@gmail.com>
2022-06-06 22:21:20 +0200

    cpu exec switching

--- src/ops/mod.rs
+++ src/ops/mod.rs
@@ -32,27 +32,34 @@ pub use random::*;
 pub fn switch_to_cpu_help_lr<
     T: GenericOCL,
     F: Fn(&InternCPU, &Matrix<T>, &Matrix<T>) -> Matrix<T>,
->(device: &InternCLDevice, lhs: &Matrix<T>, rhs: &Matrix<T>, f: F) -> Matrix<T> {
-
+>(device: &InternCLDevice, lhs: &Matrix<T>, rhs: &Matrix<T>, f: F) -> Matrix<T> 
+{
+    use custos::opencl::cpu_exec_lhs_rhs;
+    cpu_exec_lhs_rhs(device, lhs, rhs, f).unwrap()
+    /*
     let cpu = CPU::new();
     let lhs = Matrix::from((&cpu, lhs.dims(), device.read(lhs.as_buf())));
     let rhs = Matrix::from((&cpu, rhs.dims(), device.read(rhs.as_buf())));
 
     let result = f(&cpu, &lhs, &rhs);
     Matrix::from((device, result))
+    */
 }
 
 ///OpenCL
 pub fn switch_to_cpu_help_s<
     T: GenericOCL, F: 
     Fn(&InternCPU, Matrix<T>) -> Matrix<T>
->(device: &InternCLDevice, x: &Matrix<T>, f: F) -> Matrix<T> {
-    
+>(device: &InternCLDevice, x: &Matrix<T>, f: F) -> Matrix<T> 
+{
+    use custos::opencl::cpu_exec;
+    cpu_exec(device, x, f).unwrap()
+    /*
     let cpu = CPU::new();
     let x = Matrix::from((&cpu, x.dims(), device.read(x.as_buf())));
 
     let result = f(&cpu, x);
-    Matrix::from((device, result))
+    Matrix::from((device, result))*/
 }
 
 ///OpenCL
@@ -61,7 +68,12 @@ fn switch_to_cpu_help_scalar<T: Number, F: Fn(&InternCPU, Matrix<T>) -> T>(
     x: &Matrix<T>,
     f: F,
 ) -> T {
+    use custos::opencl::cpu_exec_scalar;
+    cpu_exec_scalar(device, x, f)
+    /*
     let cpu = CPU::new();
     let x = Matrix::from((&cpu, x.dims(), device.read(x.as_buf())));
     f(&cpu, x)
+    */
 }
+
