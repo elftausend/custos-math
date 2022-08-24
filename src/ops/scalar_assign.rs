d936b94d0288dbbb8b809a150ea79702d65ed8ce
elftausend <skatsacc.gut24@gmail.com>
2022-08-24 18:11:11 +0200

    Add scalar assign ops for CPU

---
+++ src/ops/scalar_assign.rs
@@ -0,0 +1,51 @@
+use std::ops::{AddAssign, DivAssign, MulAssign};
+
+use custos::{CPU, number::Number, get_device};
+
+use crate::{Matrix, assign_to_lhs_scalar};
+
+pub trait ScalarAssign<T> {
+    fn adds_assign(&self, lhs: &mut Matrix<T>, rhs: T);
+    fn muls_assign(&self, lhs: &mut Matrix<T>, rhs: T);
+    fn divs_assign(&self, lhs: &mut Matrix<T>, rhs: T);
+}
+
+impl<T> ScalarAssign<T> for CPU 
+where 
+    T: Copy + AddAssign + MulAssign + DivAssign 
+{
+    fn adds_assign(&self, lhs: &mut Matrix<T>, rhs: T) {
+        assign_to_lhs_scalar(lhs, rhs, |x, y| *x += y);
+    }
+
+    fn muls_assign(&self, lhs: &mut Matrix<T>, rhs: T) {
+        assign_to_lhs_scalar(lhs, rhs, |x, y| *x *= y);
+    }
+
+    fn divs_assign(&self, lhs: &mut Matrix<T>, rhs: T) {
+        assign_to_lhs_scalar(lhs, rhs, |x, y| *x /= y);
+    }
+}
+
+#[cfg(feature="opencl")]
+impl<T> ScalarAssign<T> for CLDevice {
+
+}
+
+impl<T: Number> AddAssign<T> for Matrix<'_, T> {
+    fn add_assign(&mut self, rhs: T) {
+        get_device!(self.device(), ScalarAssign<T>).adds_assign(self, rhs);
+    }
+}
+
+impl<T: Number> MulAssign<T> for Matrix<'_, T> {
+    fn mul_assign(&mut self, rhs: T) {
+        get_device!(self.device(), ScalarAssign<T>).muls_assign(self, rhs);
+    }
+}
+
+impl<T: Number> DivAssign<T> for Matrix<'_, T> {
+    fn div_assign(&mut self, rhs: T) {
+        get_device!(self.device(), ScalarAssign<T>).divs_assign(self, rhs);
+    }
+}
