1d3880dd68cf2c2f220ca506cf7b695f351c56e4
elftausend <skatsacc.gut24@gmail.com>
2022-08-26 23:37:49 +0200

    Simplify mse

--- src/ops/nn/loss/mse.rs
+++ src/ops/nn/loss/mse.rs
@@ -1,24 +1,22 @@
-use custos::number::Number;
+use custos::{number::Number, CDatatype};
 
 use crate::{AdditionalOps, BaseOps, Matrix, SumOps};
 
-pub fn mse<T: Copy, D: BaseOps<T> + SumOps<T>>(
-    device: &D,
+pub fn mse<T: CDatatype, D: BaseOps<T> + SumOps<T>>(
     preds: &Matrix<T>,
     targets: &Matrix<T>,
 ) -> T {
-    let x = device.sub(preds, targets);
-    device.mean(&device.mul(&x, &x))
+    let x = preds-targets;
+    (&x * &x).mean()
 }
 
-pub fn mse_grad<'a, T: Number, D: BaseOps<T> + AdditionalOps<T>>(
-    device: &'a D,
+pub fn mse_grad<T: CDatatype, D: BaseOps<T> + AdditionalOps<T>>(
     preds: &Matrix<T>,
     targets: &Matrix<T>,
-) -> Matrix<'a, T> {
-    let x = device.sub(preds, targets);
-    device.divs(
-        &device.divs(&device.muls(&x, T::two()), T::from_usize(preds.cols())),
-        T::from_usize(preds.rows()),
-    )
+) -> Matrix<T> {
+    let x = preds - targets;
+    (&x * T::two() / T::from_usize(preds.cols())) / T::from_usize(preds.rows())
+    
 }
+
+
